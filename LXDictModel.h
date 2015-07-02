ea37f0e768506da409ac78524f22f0120d8a0d7d
SnowLiu <309122873@qq.com>
2015-07-02 02:12:34 -0700

    update LXDictObject
    update

---
+++ LXDictModel.h
@@ -0,0 +1,13 @@
+//
+//  LXDictModel.h
+//  TestA
+//
+//  Created by xinliu on 15-7-2.
+//  Copyright (c) 2015年 ___xin.liu___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXDictModel : NSObject
+@property NSArray *lxArray;
+@end
---
+++ LXDictModel.m
@@ -0,0 +1,13 @@
+//
+//  LXDictModel.m
+//  TestA
+//
+//  Created by xinliu on 15-7-2.
+//  Copyright (c) 2015年 ___xin.liu___. All rights reserved.
+//
+
+#import "LXDictModel.h"
+
+@implementation LXDictModel
+
+@end
